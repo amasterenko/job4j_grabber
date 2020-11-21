88255c2114de74896a3e67f988ef109add4dfea3
Andrey Masterenko H <masterenko.as@gmail.com>
2020-11-21 21:56:13 +0300

    1.1. Job c параметрами [#365901]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,10 +3,13 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Properties;
+import java.sql.Timestamp;
 
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
@@ -18,27 +21,35 @@ public class AlertRabbit {
                 .getClassLoader().getResourceAsStream("app.properties")) {
             Properties config = new Properties();
             if (in == null) {
-                throw new FileNotFoundException();
+                throw new Exception();
             }
             config.load(in);
             int interval = Integer.parseInt(config.getProperty("rabbit.interval"));
-            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-            scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
-            SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(interval)
-                    .repeatForever();
-            Trigger trigger = newTrigger()
-                    .startNow()
-                    .withSchedule(times)
-                    .build();
-            scheduler.scheduleJob(job, trigger);
-        } catch (FileNotFoundException e) {
-            System.out.println("The 'app.properties' file was not found!");
-        } catch (IOException | NumberFormatException e) {
-            System.out.println("Wrong format of the 'app.properties' file!");
-        } catch (SchedulerException e) {
-            System.out.println("Scheduler exception was occurred!");
+            Class.forName(config.getProperty("jdbc.driver"));
+            try (Connection cn = DriverManager.getConnection(
+                    config.getProperty("jdbc.url"),
+                    config.getProperty("jdbc.username"),
+                    config.getProperty("jdbc.password"))) {
+                Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+                scheduler.start();
+                JobDataMap data = new JobDataMap();
+                data.put("connection", cn);
+                JobDetail job = newJob(Rabbit.class)
+                        .usingJobData(data)
+                        .build();
+                SimpleScheduleBuilder times = simpleSchedule()
+                        .withIntervalInSeconds(interval)
+                        .repeatForever();
+                Trigger trigger = newTrigger()
+                        .startNow()
+                        .withSchedule(times)
+                        .build();
+                scheduler.scheduleJob(job, trigger);
+                Thread.sleep(10000);
+                scheduler.shutdown();
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
         }
     }
 
@@ -46,6 +57,17 @@ public class AlertRabbit {
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
             System.out.println("Rabbit runs here ...");
+            Connection cn = (Connection) context.getJobDetail().getJobDataMap().get("connection");
+            try {
+                PreparedStatement ps = cn.prepareStatement(
+                        "INSERT INTO rabbit (created) VALUES (?) "
+                );
+                Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                ps.setTimestamp(1, timestamp);
+                ps.executeQuery();
+            } catch (SQLException e) {
+                throw new JobExecutionException(e);
+            }
         }
     }
 }
