87600a16cbce8907e9192ba7ecab3ea1b9d1a6a0
Andrey Masterenko H <masterenko.as@gmail.com>
2020-11-23 22:34:54 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#365903]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -7,20 +7,22 @@ import org.jsoup.select.Elements;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers")
-                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                        + "AppleWebKit/537.36 (KHTML, like Gecko) "
-                        + "Chrome/86.0.4240.75 Safari/537.36")
-                .get();
-        Elements rowPost = doc.select(".postslisttopic");
-        Elements rowDate = doc.select(".altCol");
-        int indx = 0;
-        for (Element td : rowPost) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            href = rowDate.get(1 + 2 * indx++);
-            System.out.println(DateParse.strToTimestamp(href.text()));
+        for (int i = 1; i <= 5; i++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i)
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                            + "AppleWebKit/537.36 (KHTML, like Gecko) "
+                            + "Chrome/86.0.4240.75 Safari/537.36")
+                    .get();
+            Elements rowPost = doc.select(".postslisttopic");
+            Elements rowDate = doc.select(".altCol");
+            int indx = 0;
+            for (Element td : rowPost) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                href = rowDate.get(1 + 2 * indx++);
+                System.out.println(DateParse.strToTimestamp(href.text()));
+            }
         }
     }
 }
